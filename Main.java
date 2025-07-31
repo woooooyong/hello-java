15330f3e324935dd58aad7d2f874865b6c512c91
김 우용 <dolphin8661@gamil.com>
2025-07-31 16:36:23 +0900

    Day 4: 클래스, 메서드, 객체 배열 반복 출력 구현

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, 우용!");
+        Member[] members = new Member[3];
+        members[0] = new Member();
+        members[0].name = "김우용";
+        members[0].age = 25;
+        members[0].email = "wooyong@example.com";
+
+        members[1] = new Member();
+        members[1].name = "이철수";
+        members[1].age = 30;
+        members[1].email = "chulsoo@example.com";
+
+        members[2] = new Member();
+        members[2].name = "박영희";
+        members[2].age = 22;
+        members[2].email = "younghee@example.com";
+
+        for (int i = 0; i < members.length; i++) {
+            System.out.println("=== " + (i + 1) + "번째 회원 ===");
+            members[i].introduce();
+            System.out.println(); // 줄바꿈
+        }
     }
 }
 
---
+++ Member.java
@@ -0,0 +1,10 @@
+public class Member {
+    String name;
+    int age;
+    String email;
+
+    void introduce() {
+        System.out.println("안녕하세요, 제 이름은 " + name + "이고 나이는 " + age + "살입니다.");
+        System.out.println("이메일은 " + email + " 입니다.");
+    }
+ }
